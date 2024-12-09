989950f277312a65cb37c9f6b93265ebe905d16b
Younes <Younes@YOUNES>
2024-12-09 23:19:27 +0300

    Add project files.

---
+++ WEB Project/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_Project.Data;
+using WEB_Project.Models;
+
+namespace WEB_Project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ WEB Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_Project.Models;
+
+namespace WEB_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "ByeBye21" },
+                new Category { Id = 2, Name = "ByeBye22" },
+                new Category { Id = 3, Name = "ByeBye23" }
+                );
+        }
+    }
+}
