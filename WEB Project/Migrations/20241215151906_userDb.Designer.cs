7c93b758cd8efd05f9c7924a071ffcbef4f47df9
Younes Rahebi <younes.rahebi@ogr.sakarya.edu.tr>
2024-12-15 18:31:30 +0300

    add-migration

--- WEB Project/Migrations/20241215151906_userDb.Designer.cs
+++ WEB Project/Migrations/20241215151906_userDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WEB_Project.Data;
 
@@ -10,9 +11,11 @@ using WEB_Project.Data;
 namespace WEB_Project.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241215151906_userDb")]
+    partial class userDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
